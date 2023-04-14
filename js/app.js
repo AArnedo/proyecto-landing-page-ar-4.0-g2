b6498be534350eef928c51c38f491a199c163f05
gustavslar <114816719+gustavslar@users.noreply.github.com>
2023-04-14 01:39:12 -0300

    agregando codigo bootstrap validacion js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,36 @@
-window.onscroll = function(){
-    if(document.documentElement.scrollTop > 100){
+
+(function () {
+    'use strict'
+
+
+    var forms = document.querySelectorAll('.needs-validation')
+
+
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 100) {
         document.querySelector('.go-top-container').classList.add('show');
-    }else{
-            document.querySelector('.go-top-container').classList.remove('show');
-        
+    } else {
+        document.querySelector('.go-top-container').classList.remove('show');
+
     }
 }
 
 document.querySelector('.go-top-container').addEventListener('click', () => {
     window.scrollTo({
-        top:0,
+        top: 0,
         behavior: 'smooth'
     })
 })
